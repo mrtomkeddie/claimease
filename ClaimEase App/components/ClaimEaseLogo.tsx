6823f327d27406874fe0760621b000d7be89ec00
Tom Keddie <thomaskeddie@gmail.com>
2025-09-15 11:42:37 +0000

    i noticed in the code theres an extra folder called claimease that i imp

--- ClaimEase App/components/ClaimEaseLogo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function ClaimEaseLogo() {
-  return (
-    <div className="w-10 h-10 bg-primary rounded-xl flex items-center justify-center">
-      <div className="text-primary-foreground font-bold text-lg">CE</div>
-    </div>
-  );
-}
